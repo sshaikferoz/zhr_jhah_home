8c6eebbad91b2b40d247cc694364cfa6e4c1c5b2
Akash R <rakash@kaartech.com>
2026-06-01 07:20:49 +0000

    Init

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["com/jhah/zhrjhahhome/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/jhah/zhrjhahhome/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
